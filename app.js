c7c4102879295fee927dd648ae170fb9be5c631b
renerodrigues01 <119670685+renerodrigues01@users.noreply.github.com>
2022-12-06 16:05:52 +0000

    Last work !

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const path = require("path");
+const bodyParser = require('body-parser')
 const app = express()
+const { MailtrapClient } = require("mailtrap");
+
+// For this example to work, you need to set up a sending domain,
+// and obtain a token that is authorized to send from the domain
+const TOKEN = "c6e1f397581692bfad15864e9b3d1283";
+const SENDER_EMAIL = "post-it@mail.com";
+const RECIPIENT_EMAIL = "renerodrigues01@protonmail.com";
+
+const client = new MailtrapClient({ token: TOKEN });
+
+const sender = { name: "Mailtrap Test", email: SENDER_EMAIL };
 
 // #############################################################################
 // Logs all request paths and method
@@ -24,9 +36,15 @@ var options = {
 }
 app.use(express.static('public', options))
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // #############################################################################
 // Catch all handler for all other request.
-app.use('*', (req,res) => {
+/*app.use('*', (req,res) => {
   res.json({
       at: new Date().toISOString(),
       method: req.method,
@@ -37,11 +55,53 @@ app.use('*', (req,res) => {
       cookies: req.cookies,
       params: req.params
     })
-    .end()
-})
+})*/
 
 app.use("/send", (req, res) =>{
-  
+  if(!req.body) {
+    res.json({message: "Body is empty"}).status(400).end()
+    return
+  }
+
+  client
+  .send({
+    category: "test",
+    custom_variables: {
+      hello: "world",
+      year: 2022,
+      anticipated: true,
+    },
+    from: sender,
+    to: [{ email: RECIPIENT_EMAIL }],
+    subject: "Hello from Mailtrap!",
+    html: `
+    <!doctype html>
+    <html>
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      </head>
+      <body style="font-family: sans-serif;">
+        <div style="display: block; margin: auto; max-width: 600px;" class="main">
+          <h1 style="font-size: 18px; font-weight: bold; margin-top: 20px">Congrats for sending test email with Mailtrap!</h1>
+          <p>Inspect it using the tabs you see above and learn how this email can be improved.</p>
+          <img alt="Inspect with Tabs" src="cid:welcome.png" style="width: 100%;">
+          <p>Now send your email using our fake SMTP server and integration of your choice!</p>
+          <p>Good luck! Hope it works. ${req.body}</p>
+        </div>
+        <!-- Example of invalid for email html/css, will be detected by Mailtrap: -->
+        <style>
+          .main { background-color: white; }
+          a:hover { border-left-width: 1em; min-height: 2em; }
+        </style>
+      </body>
+    </html>
+  `,
+  })
+  .then(console.log, console.error);
+
+  res.json({
+    message: "Message send !",
+  }).status(200)
 })
 
 module.exports = app
